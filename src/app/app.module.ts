7ad1cacb6620c3f23cada080f77360aff945eb35
Marcio Mesquita <mmesqdev@gmail.com>
2020-04-24 23:13:40 -0500

    add material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppMaterialModule } from './app-material/app-material.module';
 import { FormsModule } from '@angular/forms';
-
-/**
- * Material
- * @TODO - create its own module
- */
-/**
- * Material
- */
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -45,22 +21,7 @@ import { FilterPipe } from './filter.pipe';
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSnackBarModule,
-    MatTableModule,
-    MatToolbarModule,
-    MatButtonToggleModule,
+    AppMaterialModule,
     FlexLayoutModule,
     RouterModule.forRoot([
       { path: '**', component: HomeComponent}
